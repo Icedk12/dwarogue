767fd3894f12f8e96dce3ca61915f5d0ce9b4cae
Icedk12 <tompattonlow@gmail.com>
2026-05-15 11:49:46 +1000

    Added CameraManager to abstract the camera logic out of the game class and allow use outside of game

---
+++ base-game/src/managers/internal/CameraManager.py
@@ -0,0 +1,11 @@
+import pygame
+
+class CameraManager:
+    def __init__(self, game):
+        self.camera = pygame.Vector2((game.map_manager.width // 2) * game.map_manager.tile_size, (game.map_manager.height // 2) * game.map_manager.tile_size)
+        self.z = game.map_manager.depth // 2
+
+        self.zoom_level = 1.0
+        self.last_mouse_pos = (0, 0)
+
+        
