a94cdcac17c2fd385c07b4f36d63faebbde21dbb
Icedk12 <tompattonlow@gmail.com>
2026-05-15 13:21:29 +1000

    Added EntityManager

---
+++ base-game/src/managers/entity/EntityManager.py
@@ -0,0 +1,44 @@
+
+
+class EntityManager:
+
+    #   INFO:
+    #   Author: tompl
+    #   Status: Done until further notice
+
+    #################################################
+    ##                    INIT                     ##
+    #################################################    
+    def __init__(self):
+        self.entities = []
+        self.player = None
+
+    #################################################
+    ##                 MANAGEMENT                  ##
+    #################################################
+
+    def add(self, entity):
+        """Adds a set entity to entity list, do not use on player directly."""
+        self.entities.append(entity)
+
+    def set_player(self, player):
+        """Adds player to entity list"""
+        self.player = player
+        self.add(player)
+
+    #################################################
+    ##                    FUNC                     ##
+    #################################################
+
+    def update_turn(self, game):
+        """Calls the update_turn function for all entities and increases turn counter."""
+        for e in self.entities:
+            if e != self.player:
+                e.update_turn(game)
+
+        game.turn += 1
+
+    def draw(self, surface, game, scale=1.0):
+        """Draws all entities."""
+        for e in self.entities:
+            e.draw(surface, game, scale)
