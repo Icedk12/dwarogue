0e3cafe16afc87f2aa1fb40613dd58fbc8d791cb
Icedk12 <tompattonlow@gmail.com>
2026-05-15 11:36:26 +1000

    Added window caption to be set in GameCreateInfo

--- base-game/src/internal/setting-objects/GameCreateInfo.py
+++ base-game/src/internal/setting-objects/GameCreateInfo.py
@@ -1,4 +1,5 @@
 class GameCreateInfo:
-    def __init__(self, display_size_tuple, font):
+    def __init__(self, display_size_tuple, font, window_name):
         self.display_size = display_size_tuple
         self.font = font
+        self.window_name = window_name
