19c46efaa514cbd78ab6cf9e3fbe0e32555bea7f
Icedk12 <tompattonlow@gmail.com>
2026-05-15 16:33:24 +1000

    Implemented methods to asyncronosly generate terrain

--- base-game/src/managers/map/MapManager.py
+++ base-game/src/managers/map/MapManager.py
@@ -1,10 +1,51 @@
 import pygame
 from src.internal.createinfos.MapCreateInfo import MapCreateInfo
+from src.objects.map.Map import Map
 
 class MapManager:
-    def __init__(self, MapCreateInfo : MapCreateInfo):
-        self.map = NotImplemented
+    """Manages map storage and generation (wrapper)."""
+    #   INFO:
+    #   Author: tompl
+    #   Status: In Progress
 
+    #################################################
+    ##                    INIT                     ##
+    #################################################
 
-    def get_floor_tile_id(self, position):
-        pass # TODO: position is a pygame.Vector2 so i need to when map exists yk do it
+    def __init__(self, game,  MapCreateInfo : MapCreateInfo):
+        ###### SETUP ######
+        self.game = game
+        self.map = Map(self.game, MapCreateInfo)
+        
+        ###### GENERATION ######
+        self.world_generator = None
+        self.generating = False
+        self.generation_progress = 0.0
+
+    #################################################
+    ##                 GENERATION                  ##
+    #################################################
+
+    def start_world_generation(self):
+        """Starts async world generation"""
+
+        # Create generator
+        self.world_generator = (self.map.generate_world_async())
+
+        # Update state
+        self.generating = True
+        self.generation_progress = 0.0
+
+    def update_world_generation(self):
+        """Updates world generation."""
+        if not self.generating:
+            return # only continue if generating
+        
+        # Try to go to next generation 
+        try:
+            self.generation_progress = next(self.world_generator)
+        except StopIteration:
+
+            self.generating = False
+
+            self.generation_progress = 1.0
