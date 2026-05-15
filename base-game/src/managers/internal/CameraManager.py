3a4f1096ed1319416f49866bfd3e91e1bc74bd03
Icedk12 <tompattonlow@gmail.com>
2026-05-15 12:11:32 +1000

    Added panning bool

--- base-game/src/managers/internal/CameraManager.py
+++ base-game/src/managers/internal/CameraManager.py
@@ -5,6 +5,8 @@ class CameraManager:
         self.camera = pygame.Vector2((game.map_manager.width // 2) * game.map_manager.tile_size, (game.map_manager.height // 2) * game.map_manager.tile_size)
         self.z = game.map_manager.depth // 2
 
+        self.is_panning = False
+
         self.zoom_level = 1.0
         self.last_mouse_pos = (0, 0)
 
