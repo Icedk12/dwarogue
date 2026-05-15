6bc10fea4218cc575f09f4fd18053ce84a2aff49
Icedk12 <tompattonlow@gmail.com>
2026-05-15 14:31:04 +1000

    Added some documentation

--- base-game/src/managers/map/TileManager.py
+++ base-game/src/managers/map/TileManager.py
@@ -2,6 +2,16 @@ from src.objects.map.Tile import Tile
 from src.internal.createinfos.TileCreateInfo import TileCreateInfo
 
 class TileManager:
+    """It just is a dictionary of every tile."""
+    #   INFO:
+    #   Author: tompl
+    #   Status: Implemented 
+    #   TODO: Add JSON tile creation
+
+    #################################################
+    ##                    INIT                     ##
+    #################################################
+
     def __init__(self, game):
         self.floor_tiles = {
             1: Tile(game, TileCreateInfo("grass", True))      
